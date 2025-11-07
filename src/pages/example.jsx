90b8563979f5d2a28e2a45fc7fe74c04fa296fcb
Noha <noha.ben254@gmail.com>
2025-11-07 18:55:46 +0100

    dossier pages + page exemple

---
+++ src/pages/example.jsx
@@ -0,0 +1,10 @@
+export default function ExamplePage() {
+  return (
+    <>
+      <div className="">
+        <h1>Example Page</h1>
+        <p>This is an example page component.</p>
+      </div>
+    </>
+  );
+}
